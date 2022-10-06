88d391024b71bdb5105e47964657f2e730b27984
Mohammad Zakaria <zakariayus10@gmail.com>
2022-10-06 20:00:28 +0700

    First commit

---
+++ src/com/juaracoding/gitjava/Perhitungan.java
@@ -0,0 +1,10 @@
+package com.juaracoding.gitjava;
+
+public class Perhitungan {
+    public static void main(String[] args) {
+        int a = 12, b=10;
+        int z = a+b;
+        System.out.print(z);
+    }
+
+}
